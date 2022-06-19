25eb1babc1d17744d07e0e3e0409d428f7ef892c
Zarkwolff <diegogp11@gmail.com>
2022-06-19 17:18:15 -0300

    Update sortear_palavra.rb
    Atualização e finalização da função de sortear a palavra

--- lib/sortear_palavra.rb
+++ lib/sortear_palavra.rb
@@ -1,14 +1,15 @@
 class SortearPalavra
     def self.sortear
-      v = []
-      p = []
+
+      pa = []
+      ps = []
       
       File.open(File.expand_path('../../palavras.txt', __FILE__), 'r') do |arq|
         while line = arq.gets
-          v.push(line)
+          pa.push(line)
         end
       end
-        p = v.sample.split("")
+        ps = pa.sample.chomp.split("")
     end
   end
   
